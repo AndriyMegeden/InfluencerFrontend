89e341a0eea0a67643eb2bca6418a4c65aa12b93
Andiy <andriemeheden@gmail.com>
2024-06-24 19:03:12 +0300

    add copy input and active button to share modal

--- src/app/theme/components/modals/modals.component.ts
+++ src/app/theme/components/modals/modals.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-modals',
@@ -10,6 +17,30 @@ export class ModalsComponent {
   @Input() isOpen: boolean = false;
   @Input() modalType: string = '';
   @Output() closeModalEvent = new EventEmitter<void>();
+  @ViewChild('inputElement') inputElement!: ElementRef;
+  activeButton:
+    | 'instagram'
+    | 'tiktok'
+    | 'fasebook'
+    | 'youtube'
+    | 'more'
+    | 'social' = 'social';
+
+  setActive(value: 'instagram' | 'tiktok' | 'fasebook' | 'youtube' | 'more') {
+    this.activeButton = value;
+  }
+
+  copyLink() {
+    const inputValue = this.inputElement.nativeElement as HTMLInputElement;
+    // Выделение текста
+    inputValue.select();
+    inputValue.setSelectionRange(0, inputValue.value.length); // Для мобильных устройств
+
+    // Копирование текста в буфер обмена
+    document.execCommand('copy');
+
+    inputValue.setSelectionRange(0, 0);
+  }
 
   closeModal() {
     this.closeModalEvent.emit();
